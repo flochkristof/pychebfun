aa3e2e9f34940a0019b5f9901df869cdb979bd6b
Kristof Floch <kristof.floch@gmail.com>
2023-01-15 23:02:19 +0100

    Revert "file reorganization"
    This reverts commit 9996abb1178f2a36ba103a03b512f89293c97f73.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name         = 'pychebfun',
+    version      = "0.1.1",
+    maintainer = 'Olivier Verdier',
+    maintainer_email = 'olivier.verdier@gmail.com',
+    description  = 'Python Chebyshev Functions',
+    author = 'Olivier Verdier',
+    author_email = 'olivier.verdier@gmail.com',
+    url = "https://github.com/pychebfun/pychebfun",
+    license      = 'BSD',
+    keywords = ['Math', 'Chebyshev', 'chebfun',],
+    packages=['pychebfun',],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib'
+        ],
+    requires=[
+        'numpy',
+        'scipy',
+        'matplotlib'
+    ],
+    classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    )
